c5192af4f2cd7751866989cc48f00f6c64c00f95
lineralpha <5386041+lineralpha@users.noreply.github.com>
2024-06-06 23:46:02 -0700

    lesson 2: support login and login redirects

--- WebAuthApp/Controllers/HomeController.cs
+++ WebAuthApp/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 using WebAuthApp.Models;
 
 namespace WebAuthApp.Controllers
@@ -17,6 +20,13 @@ namespace WebAuthApp.Controllers
 
         public IActionResult Index()
         {
+            string? userName = "Unauthenticated User";
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                userName = HttpContext.User.Identity.Name;
+            }
+
+            ViewData["UserName"] = userName;
             return View();
         }
 
@@ -27,9 +37,36 @@ namespace WebAuthApp.Controllers
         }
 
         [HttpGet("login")]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl)
         {
-            return Ok("Hello");
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> ValidateAsync(string username, string password, string returnUrl)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                //return Unauthorized("Unauthorized");
+
+                // redirects to the login form view
+                ViewData["ReturnUrl"] = returnUrl;
+                TempData["ErrorMessage"] = "User authentication failure.";
+                return View("login");
+            }
+
+            // Now that user's credential is validated, we can sign in the user
+            var claims = new List<Claim>()
+            {
+                new Claim("username", username),
+                new Claim(ClaimTypes.NameIdentifier, username),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(claimsPrincipal);
+
+            return Redirect(returnUrl);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
